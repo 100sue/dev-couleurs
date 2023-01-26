231d680adf1dea2a90d47bcbcc8bdc8aa0f6adf0
appert <scoreur@gmail.com>
2023-01-26 22:46:16 +0100

    ajout js

---
+++ main.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector('button');
+const color = document.querySelector('.color');
+const hex = document.querySelector('.hex');
+
+btn.addEventListener('click', generateColors);
+
+
+function generateColors() {
+    for(let i = 0; i < color.length; i++) {
+  // Generer une couleur au hasard :      
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  // Ajouter la couleur au background:  
+        color[i].style.background = "#" + randomColor;
+  // Ajouter l'animation : 
+        color[i].classList.add('fade-in');
+        setTimeout(() => color[i].classList.remove('fade-in'), 400);
+ // Ajouter le code hex: 
+        hex[i].innerHTML = randomColor;
+    }
+}
+
+generateColors();
